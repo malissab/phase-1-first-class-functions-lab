627afe849496ccb106f892f52a18f083ca1f46ee
Malissa Bishop <malissambishop7@gmail.com>
2021-10-13 21:57:29 -0700

    commit tree 535c9160ce8554b2a526486d4736d0dc10680b45

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    const firstTwoDrivers = drivers.slice(0, 2);
+    return firstTwoDrivers;
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    const LastTwoDrivers = drivers.slice(-2);
+    return LastTwoDrivers;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier() {
+    return function(fare) {
+        return fare ** 2;
+    }
+}
+const fareDoubler = function (fare) {
+    return fare * 2;
+}
+
+const fareTripler = function (fare) {
+    return fare * 3;
+}
+
+const selectDifferentDrivers = function (arrayOfDrivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(arrayOfDrivers);
+}
